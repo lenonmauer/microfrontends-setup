7fc986e5277d809519a36e79f2277b049342dcf9
Lenon Mauer <lenonmauer0@gmail.com>
2022-06-25 17:36:46 -0300

    shell

---
+++ apps/shell/next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    const { ModuleFederationPlugin } = options.webpack.container
+
+    config.plugins.push(
+      new ModuleFederationPlugin({
+        name: 'shell',
+        remotes: {
+          '@mfe/home': 'home@http://localhost:3001/remoteEntry.js',
+          '@mfe/auth': 'auth@http://localhost:3002/remoteEntry.js',
+        },
+        shared: [],
+      })
+    )
+
+    return config
+  },
+}
+
+module.exports = nextConfig
---
+++ apps/shell/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <div>Next.js Shell</div>
+}
+
+export default Home
